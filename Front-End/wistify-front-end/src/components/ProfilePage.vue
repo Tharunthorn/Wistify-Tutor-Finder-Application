3e7f1ca56fc5e1d40fc09ce31e6e188f3b550731
KhongnatBaibua <khongnat.b@ku.th>
2023-11-10 17:25:58 +0700

    Profile pic now displaying correctly.

--- Front-End/wistify-front-end/src/components/ProfilePage.vue
+++ Front-End/wistify-front-end/src/components/ProfilePage.vue
@@ -3,13 +3,16 @@
     <Navbar />
     <div class="page-background"></div>
     <div class="square-layout">
-      <div class="profile-pic">
-        <label class="-label" for="file">
-          <span class="glyphicon glyphicon-camera"></span>
-          <span>Change Image</span>
-        </label>
-        <input id="file" type="file" @change="loadFile" />
-        <img v-if="imageSrc" :src="imageSrc" id="output" width="200" height="200" />
+      <div class="container">
+        <div class="avatar-upload">
+          <div class="avatar-edit">
+            <input type="file" id="imageUpload" accept=".png, .jpg, .jpeg" @change="readURL" />
+            <label for="imageUpload"></label>
+          </div>
+          <div class="avatar-preview">
+            <div :style="{ 'background-image': 'url(' + imageSrc + ')' }"></div>
+          </div>
+        </div>
       </div>
     </div>
     <div class="footer">
@@ -27,28 +30,33 @@ export default {
   },
   data() {
     return {
-      imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg',
+      imageSrc: '/public/Default_pfp.svg',
     };
   },
   methods: {
-    loadFile(event) {
-      this.imageSrc = URL.createObjectURL(event.target.files[0]);
+    readURL(event) {
+      const input = event.target;
+      if (input.files && input.files[0]) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          this.imageSrc = e.target.result;
+        };
+        reader.readAsDataURL(input.files[0]);
+      }
     },
   },
 };
 </script>
 
 <style scoped>
-/* Import Montserrat font from Google Fonts */
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+/* Import fonts as needed */
 
 .page-background {
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
-  background-image: url("public/Background.svg");
-  opacity: 0.3;
+  background-color: #F6F6F6;
   background-repeat: no-repeat;
   background-size: cover;
   display: flex;
@@ -67,8 +75,9 @@ export default {
   height: 350px;
   flex-shrink: 0;
   border-radius: 40px;
-  background: green;
+  background: #0D0C0C;
   align-items: center;
+  /* Add additional styling for the square layout as needed */
 }
 
 .footer {
@@ -82,55 +91,90 @@ export default {
 }
 
 .footer-text {
-  color: #000000;
+  color: #000000; /* Text color */
   margin-top: 4rem;
   text-align: center;
   font-family: 'Montserrat', sans-serif;
-  font-size: 0.8rem;
+  font-size: 0.8rem; /* Adjust the font size */
 }
 
-.profile-pic {
+body {
+  background: whitesmoke;
+  font-family: 'Open Sans', sans-serif;
+}
+.container {
+  max-width: 960px;
+  width: 430px;
+}
+h1 {
+  font-size: 20px;
+  text-align: center;
+  margin: 20px 0 20px;
+}
+h1 small {
+  display: block;
+  font-size: 15px;
+  padding-top: 8px;
+  color: gray;
+}
+.avatar-upload {
   position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  input {
-    display: none;
-  }
-
-  img {
-    position: absolute;
-    object-fit: cover;
-    width: 165px;
-    height: 165px;
-    box-shadow: 0 0 10px 0 rgba(255, 255, 255, .35);
-    border-radius: 100px;
-    z-index: 0;
-  }
-
-  .-label {
-    cursor: pointer;
-    height: 165px;
-    width: 165px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(0, 0, 0, 0.8);
-    color: rgb(250, 250, 250);
-    border-radius: 100px;
-    opacity: 0; /* Initially hide the label */
-    transition: opacity 0.2s ease-in-out;
-  }
-
-  &:hover .-label {
-    opacity: 1; /* Show the label on hover */
-  }
-
-  span {
-    display: inline-flex;
-    padding: 0.2em;
-    height: 2em;
-  }
+  max-width: 205px;
+  margin: 45px auto;
 }
+.avatar-upload .avatar-edit {
+  position: absolute;
+  right: -20px;
+  z-index: 1;
+  top: 10px;
+}
+.avatar-upload .avatar-edit input {
+  display: none;
+}
+.avatar-upload .avatar-edit input + label {
+  display: inline-block;
+  width: 34px;
+  height: 34px;
+  margin-bottom: 0;
+  border-radius: 100%;
+  background: #FFFFFF;
+  border: 1px solid transparent;
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.12);
+  cursor: pointer;
+  font-weight: normal;
+  transition: all 0.2s ease-in-out;
+}
+.avatar-upload .avatar-edit input + label:hover {
+  background: #f1f1f1;
+  border-color: #d6d6d6;
+}
+.avatar-upload .avatar-edit input + label:after {
+  content: "\f040";
+  font-family: 'FontAwesome';
+  color: #757575;
+  position: absolute;
+  top: 10px;
+  left: 0;
+  right: 0;
+  text-align: center;
+  margin: auto;
+}
+.avatar-upload .avatar-preview {
+  width: 250px;
+  height: 250px;
+  position: relative;
+  border-radius: 100%;
+  border: 6px solid #F8F8F8;
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.1);
+}
+.avatar-upload .avatar-preview > div {
+  width: 100%;
+  height: 100%;
+  border-radius: 100%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+/* Add more styles as needed for different profile sections */
 </style>
