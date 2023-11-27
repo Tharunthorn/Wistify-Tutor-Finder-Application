2098f44453be24a64c9ef1e52f924743cbba08f4
Tharunthorn Musik <tharunthornmusik1446@gmail.com>
2023-11-27 23:48:38 +0700

    add STATIC_ROOT

--- django_website/settings.py
+++ django_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2vs$g^int$(3q4t69=&n=zh1vrhzmi=-13or&9vg9cm^&hxvf-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://regular-civet-learning.ngrok-free.app', '*']
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -153,6 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'main/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
